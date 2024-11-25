4c075c80d8e8ff9ea5a02228dffcb0415afd892e
GhostSubmarine <1073944897@qq.com>
2024-11-25 17:18:57 +0800

    更改下布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,74 @@
 import "./App.css";
 import { ProChat } from '@ant-design/pro-chat'
+import { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
 
+const { Header, Sider, Content } = Layout;
 const App = () => {
-
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <ProChat
-      request={async (messages) => {
-        // Send a request with Message as the parameter
-        return messages; // Supports both streaming and non-streaming
-      }}
-    />
+    <Layout style={{
+      height: '100%',
+      overflow: 'auto'
+    }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content>
+          <ProChat
+            request={async (messages) => {
+              // Send a request with Message as the parameter
+              return messages; // Supports both streaming and non-streaming
+            }}
+          />
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
